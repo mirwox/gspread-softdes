f77f81b8776c73c08791fac5263db40c4a1f94ac
Fabio de Miranda <fmiranda@gmail.com>
2015-05-24 19:50:34 -0300

    Todos os exemplos rodando

--- gspread_oauth.py
+++ gspread_oauth.py
@@ -1,6 +1,3 @@
-__author__ = 'mirwox'
-
-
 from oauth2client.client import SignedJwtAssertionCredentials
 import json
 import gspread
@@ -14,17 +11,19 @@ json_key = json.load(open('json_app_id.json'))
 scope = ['https://spreadsheets.google.com/feeds']
 
 credentials = SignedJwtAssertionCredentials(
-            json_key['client_email']
-            , bytes(json_key['private_key'], 'UTF-8')
-            , scope)
+    json_key['client_email']
+    , bytes(json_key['private_key'], 'UTF-8')
+    , scope)
 
 gc = gspread.authorize(credentials)
 
 # A partir daqui continua igual
 
 # Abre uma aba da planilha
-#wks = gc.open("Planilha Teste").sheet1
-wks = gc.open_by_url("https://docs.google.com/spreadsheets/d/1OwYU05sxy2iH5_vOzMQgVMLaQB673DuVJ3BpQfY4Txc/edit#gid=0")
+wks = gc.open("Planilha Teste").sheet1
+
+# Pode-se também abrir pela URL
+# wks = gc.open_by_url("https://docs.google.com/spreadsheets/d/1OwYU05sxy2iH5_vOzMQgVMLaQB673DuVJ3BpQfY4Txc/edit#gid=0")
 
 
 wks.update_acell('B3', "Teste na planilha")
